0a0181f43bc1b106f932f466d280063ce95d325f
Stephanie Hansen <stephanie@stephanie.dev>
2020-09-08 04:10:56 +0100

    feat: import shortener service

--- src/app/components/shortener/shortener.component.ts
+++ src/app/components/shortener/shortener.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ShortenerService } from '../../services/shortener.service';
+
 @Component({
   selector: 'app-shortener',
   templateUrl: './shortener.component.html',
   styleUrls: ['./shortener.component.scss']
 })
 export class ShortenerComponent implements OnInit {
+  url: string;
 
-  constructor() { }
+  constructor(private shortenerService: ShortenerService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.shortenerService.getShortenedLinks(this.url);
   }
 
 }
