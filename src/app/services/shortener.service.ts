3ca552d4dd185597aa9eae7a96a131a42ac01942
Stephanie Hansen <stephanie@stephanie.dev>
2020-09-08 17:38:26 +0100

    chore: update method name

--- src/app/services/shortener.service.ts
+++ src/app/services/shortener.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ShortenerService {
 
   constructor(private http: HttpClient) { }
 
-  getShortenedLinks(url: string) {
+  getShortenedLink(url: string) {
     return this.http.post(this.shortenerUrl, {"url": url})
   }
 }
