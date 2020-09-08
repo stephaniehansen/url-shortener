fb8863d1f7366fb0ba1d0e3863f7787a3a747299
Stephanie Hansen <stephanie@stephanie.dev>
2020-09-08 07:13:29 +0100

    feat: create shortened link via api

--- src/app/services/shortener.service.ts
+++ src/app/services/shortener.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ShortenerService {
 
-  constructor() { }
+export class ShortenerService {
+  private shortenerUrl = 'https://rel.ink/api/links/';
+
+  constructor(private http: HttpClient) { }
+
+  getShortenedLinks(url: string) {
+    return this.http.post(this.shortenerUrl, {"url": url})
+  }
 }
