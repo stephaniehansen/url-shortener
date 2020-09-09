29907366b01bd65b1fae1ae40b08d4eb94f1a44e
Stephanie Hansen <stephanie@stephanie.dev>
2020-09-09 06:40:24 +0100

    feat: add getLinks method, handle api response

--- src/app/services/shortener.service.ts
+++ src/app/services/shortener.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+
+import { Link } from '../models/link';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,20 @@ import { Observable } from 'rxjs';
 
 export class ShortenerService {
   private shortenerUrl = 'https://rel.ink/api/links/';
+  private links: Link[] = [];
 
   constructor(private http: HttpClient) { }
 
-  getShortenedLink(url: string) {
+  getLinks(): Observable<Link[]> {
+    return of (this.links);
+  }
+
+  addLink(url: string) {
     return this.http.post(this.shortenerUrl, {"url": url})
+    .subscribe(response => {
+      this.links.unshift({
+        shortUrl: "https://rel.ink/" + response[Object.keys(response)[0]], 
+        longUrl: url})
+    });
   }
 }
